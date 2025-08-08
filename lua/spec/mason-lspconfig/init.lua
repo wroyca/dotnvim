ad36c394ff1959b2a1fb35f4d360573ef4e3647c
William Roy <wroy@proton.me>
2025-08-08 13:38:40 -0400

    Add dynamic language server discovery for mason-lspconfig

--- lua/spec/mason-lspconfig/init.lua
+++ lua/spec/mason-lspconfig/init.lua
@@ -1,14 +1,47 @@
 ---@module "mason-lspconfig"
 
+---@return string[] declared_language_servers
+local function language_servers ()
+  local config_dir = vim.fs.normalize (vim.fn.stdpath ("config") .. "/lsp")
+
+  if vim.fn.isdirectory (config_dir) ~= 1 then
+    return {}
+  end
+
+  local glob_pattern = vim.fs.normalize (config_dir .. "/*.lua")
+  local module_paths = vim.fn.glob (glob_pattern, false, true)
+
+  return vim
+    .iter (module_paths)
+    :map (function (path)
+      return vim.fn.fnamemodify (path, ":t:r")
+    end)
+    :totable ()
+end
+
+---@type string[]
+local additional_language_servers = {
+  "lua_ls",
+}
+
+---@return string[] language_servers_registry
+local function language_servers_registry ()
+  local all_servers = vim.iter ({ language_servers (), additional_language_servers }):flatten ():totable ()
+
+  return vim.iter (all_servers):fold ({}, function (acc, identifier)
+    if not vim.tbl_contains (acc, identifier) then
+      table.insert (acc, identifier)
+    end
+    return acc
+  end)
+end
+
 ---@type LazyPluginSpec
 local Spec = {
-  "mason-org/mason-lspconfig.nvim", event = "VeryLazy", dependencies = "mason.nvim",
+  "mason-org/mason-lspconfig.nvim", event = "VeryLazy",
 
   opts = {
-    ensure_installed = {
-      "lua_ls",
-      "clangd",
-    },
+    ensure_installed = language_servers_registry (),
   },
 }
 
